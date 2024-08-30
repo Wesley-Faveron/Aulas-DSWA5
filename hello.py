9edde3457541ba502fa09a65e0291aba483395f0
Wesley Faveron de Almeida <134218678+Wesley-Faveron@users.noreply.github.com>
2024-08-30 20:43:04 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,35 +1,61 @@
-from flask import Flask, render_template, session, redirect, url_for, flash, request
+import os
+from datetime import datetime
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, PasswordField
+from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired
-from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+class Disciplina(db.Model):
+    __tablename__ = 'disciplinas'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    professores = db.relationship('Professor', backref='disciplina', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Disciplina %r>' % self.name
+
+class Professor(db.Model):
+    __tablename__ = 'professores'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(64), unique=True, index=True)
+    disciplina_id = db.Column(db.Integer, db.ForeignKey('disciplinas.id'))
+
+    def __repr__(self):
+        return '<Professor %r>' % self.nome
 
 class NameForm(FlaskForm):
-    name = StringField('Informe o seu nome', validators=[DataRequired()])
-    surname = StringField('Informe o seu sobrenome:', validators=[DataRequired()])
-    institution = StringField('Informe a sua Insituição de ensino:', validators=[DataRequired()])
-    discipline = SelectField(u'Informe a sua disciplina:', choices=[('dswa5', 'DSWA5'), ('dwba4', 'DWBA4'), ('GPSA5', 'Gestão de projetos')])
-    submit = SubmitField('Submit')
+    name = StringField('Cadastre o novo Professor:', validators=[DataRequired()])
+    role = SelectField(u'Disciplina associada:', coerce=int)
+    submit = SubmitField('Cadastrar')
 
-class LoginForm(FlaskForm):
-    username = StringField('Informe o seu usuário', validators=[DataRequired()])
-    password = PasswordField('Informe a sua senha:', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
+    def __init__(self, *args, **kwargs):
+        super(NameForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(disciplina.id, disciplina.name) for disciplina in Disciplina.query.order_by(Disciplina.name).all()]
 
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, Professor=Professor, Disciplina=Disciplina)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
 
 @app.errorhandler(500)
 def internal_server_error(e):
@@ -38,51 +64,47 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    current_time = datetime.utcnow()
+    return render_template('index.html', current_time=current_time)
+
+    
+@app.route('/disciplinas', methods=['GET', 'POST'])
+def disciplinas():
+    return render_template('NaoDis.html')
+    
+@app.route('/alunos', methods=['GET', 'POST'])
+def alunos():
+    return render_template('NaoDis.html')
+
+@app.route('/professores', methods=['GET', 'POST'])
+def professores():
     form = NameForm()
+    professores_all = Professor.query.all()
+    disciplinas_all = Disciplina.query.all()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Você alterou o seu nome!')
+        professor = Professor.query.filter_by(nome=form.name.data).first()
+        if professor is None:
+            disciplina_associada = Disciplina.query.get(form.role.data)
+            professor = Professor(nome=form.name.data, disciplina=disciplina_associada)
+            db.session.add(professor)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
-        session['surname'] = form.surname.data
-        session['institution'] = form.institution.data
-        session['discipline'] = form.discipline.data
-        session['remote_addr'] = request.remote_addr;
-        session['host'] = request.host;
-        return redirect(url_for('index'))
+        return redirect(url_for('professores'))
+    return render_template('professores.html', form=form, name=session.get('name'),
+                           known=session.get('known', False),
+                           professores_all=professores_all, disciplinas_all=disciplinas_all)
 
-    return render_template('index.html',
-                           form=form,
-                           name=session.get('name'),
-                           surname=session.get('surname'),
-                           institution=session.get('institution'),
-                           discipline=session.get('discipline'),
-                           choices=dict(form.discipline.choices),
-                           remote_addr=session.get('remote_addr'),
-                           remote_host=session.get('host'),
-                           current_time=datetime.utcnow())
+def create_disciplines():
+    disciplinas = ['DSWA5', 'GPSA5', 'IHCA5', 'SODA5', 'PJIA5', 'TCOA5']
+    for nome in disciplinas:
+        if not Disciplina.query.filter_by(name=nome).first():
+            db.session.add(Disciplina(name=nome))
+    db.session.commit()
 
-@app.route('/loginResponse', methods=['GET'])
-def loginResponse():
-    return render_template('loginResponse.html',
-                           username=session.get('username'),
-                           password=session.get('password'),
-                           remote_addr=session.get('remote_addr'),
-                           remote_host=session.get('host'),
-                           current_time=datetime.utcnow())
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    formLogin = LoginForm()
-    if formLogin.validate_on_submit():
-        session['username'] = formLogin.username.data
-        session['password'] = formLogin.password.data
-        session['remote_addr'] = request.remote_addr;
-        session['host'] = request.host;
-        return redirect(url_for('loginResponse'))
-
-    return render_template('login.html',
-                           form=formLogin,
-                           remote_addr=request.remote_addr,
-                           remote_host=request.remote_addr,
-                           current_time=datetime.utcnow())
+if __name__ == '__main__':
+    with app.app_context():
+        create_disciplines()
+    app.run(debug=True)
